3bebdf2d4710a35d93c919b846ac01d5a8c883db
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-10-23 16:15:19 +0700

    Add folder named Buoi6

---
+++ src/Buoi6/Buoi4.java
@@ -0,0 +1,48 @@
+package Buoi6;
+
+import java.util.Scanner;
+
+public class Buoi4 {
+    public static void bai4_1() {
+        int arr[]={3, 5, 8, 10, 14, 25};
+        System.out.print("\nDanh sach phan tu cua mang la");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("\nPhan tu arr[%d] la: %d", i, arr[i]);
+        }
+    }
+
+    public static void bai4_2() {
+        String nguHanh[]={"Kim", "Moc", "Thuy", "Hoa", "Tho"};
+        for (int i = 0; i < nguHanh.length; i++) {
+            System.out.printf("\nNgu hanh [%d] la: %s", i, nguHanh[i]);
+        }
+    }
+
+    public static void bai4_2NC(int n) {
+        System.out.printf("\n\nDanh sach cac phan tu tu 1 den %d: ", n);
+        for (int i = 1; i <= n; i++) {
+            System.out.print("\t" + i);
+        }
+    }
+
+    public static void bai4_3NC(int n) {
+        System.out.printf("\n\nDanh sach cac phan tu nho hon hoac bang %d la: ", n);
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print("\t" + i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.print("Nhap so phan tu cua mang: ");
+        num = sc.nextInt();
+
+        bai4_1();
+        bai4_2();
+        bai4_2NC(num);
+        bai4_3NC(num);
+    }
+}
