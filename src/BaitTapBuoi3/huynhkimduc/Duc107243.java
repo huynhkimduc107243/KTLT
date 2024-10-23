03e0f44f13a3eee25d21fe52044d3104b6ba5615
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-10-23 14:07:29 +0700

    Format code

--- src/BaitTapBuoi3/huynhkimduc/Duc107243.java
+++ src/BaitTapBuoi3/huynhkimduc/Duc107243.java
@@ -24,44 +24,43 @@ public class Duc107243 {
         System.out.println("Khoa: " + khoa);
         System.out.println("-------------KET-------------");
     }
+
     public static void Ex2(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Nhap ma sinh vien: ");
-            int maSinhVien = scanner.nextInt();
-            scanner.nextLine(); // Sử dụng dòng mới
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap ma sinh vien: ");
+        int maSinhVien = scanner.nextInt();
+        scanner.nextLine(); // Sử dụng dòng mới
 
-            System.out.print("Nhap tuoi: ");
-            int tuoi = scanner.nextInt();
-            scanner.nextLine(); // Sử dụng dòng mới
+        System.out.print("Nhap tuoi: ");
+        int tuoi = scanner.nextInt();
+        scanner.nextLine(); // Sử dụng dòng mới
 
-            System.out.print("Nhap gioi tinh (Nam: true / Nu: false): ");
-            boolean gioiTinh = scanner.nextBoolean();
-            scanner.nextLine(); // Sử dụng dòng mới
+        System.out.print("Nhap gioi tinh (Nam: true / Nu: false): ");
+        boolean gioiTinh = scanner.nextBoolean();
+        scanner.nextLine(); // Sử dụng dòng mới
 
-            System.out.print("Nhap ho va ten: ");
-            String hoVaTen = scanner.nextLine();
+        System.out.print("Nhap ho va ten: ");
+        String hoVaTen = scanner.nextLine();
 
-            System.out.print("Nhap ngay sinh: ");
-            String ngaySinh = scanner.nextLine();
+        System.out.print("Nhap ngay sinh: ");
+        String ngaySinh = scanner.nextLine();
 
-            System.out.print("Nhap lop: ");
-            String lop = scanner.nextLine();
+        System.out.print("Nhap lop: ");
+        String lop = scanner.nextLine();
 
-            System.out.print("Nhap khoa: ");
-            String khoa = scanner.nextLine();
+        System.out.print("Nhap khoa: ");
+        String khoa = scanner.nextLine();
 
-            System.out.print("Nhap dia chi: ");
-            String diaChi = scanner.nextLine();
+        System.out.print("Nhap dia chi: ");
+        String diaChi = scanner.nextLine();
 
-            System.out.println();
-            System.out.print("-----THONG TIN SINH VIEN-----");
-            System.out.printf(
-                    "\nMa sinh vien: %d\nHo va ten: %s\nNgay sinh: %s\nTuoi: %d\nGioi tinh: %s\nLop: %s \nKhoa: %s\nDia chi: %s\n",
-                    maSinhVien, hoVaTen, ngaySinh, tuoi, gioiTinh(gioiTinh), lop, khoa, diaChi);
-            System.out.println("-------------KET-------------");
-        }
+        System.out.println();
+        System.out.print("-----THONG TIN SINH VIEN-----");
+        System.out.printf(
+                "\nMa sinh vien: %d\nHo va ten: %s\nNgay sinh: %s\nTuoi: %d\nGioi tinh: %s\nLop: %s \nKhoa: %s\nDia chi: %s\n",
+                maSinhVien, hoVaTen, ngaySinh, tuoi, gioiTinh(gioiTinh), lop, khoa, diaChi);
+        System.out.println("-------------KET-------------");
     }
-
 
     public static void main(String[] args) {
         Ex1(args);
