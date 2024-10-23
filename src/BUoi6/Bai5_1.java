1cbd6e4a37a4d733a38d8a6b5772693d6ccbd6f1
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-10-23 14:41:40 +0700

    Add file Bai5_1.java, Bai5_2.java and Buoi4.java

---
+++ src/BUoi6/Bai5_1.java
@@ -0,0 +1,43 @@
+package Buoi6;
+
+import java.util.Scanner;
+
+public class Bai5_1 {
+    // SỬ dụng toán tử 3 ngôi - ternary operator
+    public static int timGiaTriLonNhat(int num1, int num2) {
+        return (num1 > num2 ? num1 : num2);
+    }
+
+    public static int timGiaTriNhoNhat(int num1, int num2) {
+        return (num1 < num2 ? num1 : num2);
+    }
+
+    // Sử dụng phương pháp so sánh với if
+    public static int soLonNhat(int num1, int num2) {
+        int max = num1;
+        if (max < num2)
+            max = num2;
+            return max;
+    }
+    public static int soNhoNhat(int num1, int num2) {
+        int min = num1;
+        if (min > num2)
+            min = num2;
+            return min;
+        
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+        System.out.print("Nhap so thu nhat: ");
+        num1 = sc.nextInt();
+        System.out.print("Nhap so thu hai: ");
+        num2 = sc.nextInt();
+        sc.close();
+        System.out.printf("\nGia tri lon nhat trong 2 so %d va %d la: %d", num1, num2, timGiaTriLonNhat(num1, num2));
+        System.out.printf("\nGia tri nho nhat trong 2 so %d va %d la: %d", num1, num2, timGiaTriNhoNhat(num1, num2));
+        System.out.printf("\nGia tri lon nhat trong 2 so %d va %d la: %d", num1, num2, soLonNhat(num1, num2));
+        System.out.printf("\nGia tri nho nhat trong 2 so %d va %d la: %d", num1, num2, soNhoNhat(num1, num2));
+    }
+}
---
+++ src/BUoi6/Buoi4.java
@@ -0,0 +1,15 @@
+package Buoi6;
+
+public class Buoi4 {
+    public static void bai4_1() {
+        int arr[]={3, 5, 8, 10, 14, 25};
+        System.out.print("Danh sach phan tu cua mang la: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("\nPhan tu arr[%d] la: %d", i, arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        bai4_1();
+    }
+}
