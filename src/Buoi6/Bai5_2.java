4f7f5a195d6f917f5a7ce6f8e9ce64512237aa7a
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-10-28 16:26:53 +0700

    Edit comments in Bai5_2.java

--- src/Buoi6/Bai5_2.java
+++ src/Buoi6/Bai5_2.java
@@ -5,70 +5,71 @@ public class Bai5_2 {
         int a = 3;
 
         System.out.println("Ex 1:");
-        // a++/a-- tăng/giảm sau, ++/--a tăng/giảm trước,
-        System.out.println(a++); // In ra a ban đầu rồi +1 = 3
-        System.out.println(++a); // 3 + 1 rồi +1 = 5
-        System.out.println(a++ + 1); // 5 + 1 = 6 rồi +1 = 7
-        System.out.println(++a + 1); // 7 + 1 = 8
+        System.out.println(a++); // 3
+        System.out.println(++a); // 5
+        System.out.println(a++ + 1); // 6
+        System.out.println(++a + 1); // 8
     }
 
     public static void viDu2() {
         int a = 0, b, c;
-        a++; // a = 1 sau đó a = 0 + 1
-        b = a; // gán b = a
-        c = a++; // gán c = a sau đó a + 1 = 2
+        a++;
+        b = a; // b = 1
+        c = a++; // c = 1, a = 2
 
         System.out.println("\nEx 2:");
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println(a); // 2
+        System.out.println(b); // 1
+        System.out.println(c); // 1
     }
 
     public static void viDu3() {
         int a = 0, b, c;
-        ++a; // a = 0 sau đó a = 0 + 1
-        b = a; // gán b = a = 1
-        c = ++a; // gán c = a + 1 = 2, gán lại a = 2
+        ++a; // a = 1
+        b = a; // b = 1
+        c = ++a; // c = 2, a = 2
 
         System.out.println("\nEx 3:");
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println(a); // 2
+        System.out.println(b); // 1
+        System.out.println(c); // 2
     }
 
     public static void viDu4() {
         int a = 0, b, c;
-        ++a; // a = 0 -> 1
-        b = ++a; // gán b = a + 1 = 2
-        c = ++a; // gán c = a + 1 = 3
+        ++a; // a = 1
+        b = ++a; // b = 2, a = 2
+        c = ++a; // c = 3, a = 3
 
-        System.out.println("\nEx4:");
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println("\nEx 4:");
+        System.out.println(a); // 3
+        System.out.println(b); // 2
+        System.out.println(c); // 3
     }
 
     public static void viDu5() {
         int a = 0, b, c;
-        ++a; // a = 0, sau đó a + 1
-        b = ++a; // gán b = a + 1 = 2
-        c = b++; // c = b = 2, sau đó b + 1 = 3
-        System.out.println("\nEx5:");
-        System.out.println(a); // a = 0 -> 1
-        System.out.println(b); // gán b = 1 + 1 = 2, a = 2
-        System.out.println(c); // gán c = b = 2 rồi +1
+        ++a; // a = 1
+        b = ++a; // b = 2, a = 2
+        c = b++; // c = 2, b = 3
+
+        System.out.println("\nEx 5:");
+        System.out.println(a); // 2
+        System.out.println(b); // 3
+        System.out.println(c); // 2
     }
 
     public static void viDu6() {
         int a = 0, b, c;
-        ++a; // a = 0 -> 1
-        b = a++; // gán b = a = 1, a = 1 + 1 = 2
-        c = b++; // c = 1, b + 1 = 2 gán cho b
-        c += c; // c += c = 1 + 1 = 2
-        System.out.println("\nEx6:");
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        ++a; // a = 1
+        b = a++; // b = 1, a = 2
+        c = b++; // c = 1, b = 2
+        c += c; // c = 2
+
+        System.out.println("\nEx 6:");
+        System.out.println(a); // 2
+        System.out.println(b); // 2
+        System.out.println(c); // 2
     }
 
     public static void main(String[] args) {
