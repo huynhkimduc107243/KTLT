665df0a067274f0f05ae0a5a367359ef79ddc0b9
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-10-30 13:15:11 +0700

    Reformat bracket

--- src/Buoi6/Buoi4.java
+++ src/Buoi6/Buoi4.java
@@ -9,7 +9,8 @@ public class Buoi4
         int arr[] = { 3, 5, 8, 10, 14, 25 };
         System.out.print("\nBai 4.1/ Danh sach phan tu cua mang la:");
         // khi i < độ dài của mảng thì in ra phần tử tương ứng sau đó tăng i thêm 1 đến khi i > độ dài của mảng
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
+        {
             System.out.printf("\nPhan tu arr[%d] = %d", i, arr[i]);
         }
     }
@@ -19,7 +20,8 @@ public class Buoi4
         String nguHanh[] = { "Kim", "Moc", "Thuy", "Hoa", "Tho" };
         System.out.print("\n\nBai 4.2/ Danh sach cac ngu hanh la:");
         // khi i < độ dài của mảng thì in ra phần tử tương ứng sau đó tăng i thêm 1 đến khi i > độ dài của mảng
-        for (int i = 0; i < nguHanh.length; i++) {
+        for (int i = 0; i < nguHanh.length; i++)
+        {
             System.out.printf("\nPhan tu thu [%d] la: %s", i, nguHanh[i]);
         }
     }
@@ -30,9 +32,11 @@ public class Buoi4
         int count = 1; // đếm số thứ tự phần tử
         System.out.print("\n\nBai 4.3/ Danh sach cac phan tu cua mang arr1 la:");
         // khi i < số hàng của mảng thì in ra số hàng tại i sau đó tăng i thêm 1 đến khi i > số hàng của mảng
-        for (int i = 0; i < arr1.length; i++) {
+        for (int i = 0; i < arr1.length; i++)
+        {
             // khi j < số cột của mảng thì in ra số cột tại j sau đó tăng j thêm 1 đến khi j> số cột của mảng
-            for (int j = 0; j < arr1[i].length; j++) {
+            for (int j = 0; j < arr1[i].length; j++)
+            {
                 System.out.printf("\nPhan tu thu %d: arr1[%d][%d]", count++, i, j);
             }
         }
@@ -44,9 +48,11 @@ public class Buoi4
         int count = 1; // đếm số thứ tự phần tử
         System.out.print("\n\nBai 4.4/ Danh sach cac phan tu cua mang arr2 la:");
         // khi i < số hàng của mảng thì in ra số hàng tại i sau đó tăng i thêm 1 đến khi i > số hàng của mảng
-        for (int i = 0; i < arr2.length; i++) {
+        for (int i = 0; i < arr2.length; i++)
+        {
             // khi j < số cột của mảng thì in ra số cột tại j sau đó tăng j thêm 1 đến khi j> số cột của mảng
-            for (int j = 0; j < arr2[i].length; j++) {
+            for (int j = 0; j < arr2[i].length; j++)
+            {
                 System.out.printf("\nPhan tu thu %d la arr2[%d][%d] = %d", count++, i, j, arr2[i][j]);
             }
         }
@@ -56,7 +62,8 @@ public class Buoi4
     {
         System.out.printf("\n\nBai 4.2NC/ Danh sach cac phan tu tu 1 den %d: ", n);
         // khi i <= n thì in ra phần tử tương ứng sau đó tăng i thêm 1 đến khi i > n
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
+        {
             System.out.print("\t" + i);
         }
     }
@@ -65,8 +72,10 @@ public class Buoi4
     {
         System.out.printf("\n\nBai 4.3NC/ Danh sach cac so le nho hon hoac bang %d la: ", n);
         // khi i <= n thì kiểm tra i có phải số lẻ không, nếu có thì in ra sau đó i tăng thêm 1 đến khi i > n
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 != 0) {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 != 0)
+            {
                 System.out.print("\t" + i);
             }
         }
@@ -76,8 +85,9 @@ public class Buoi4
     {
         System.out.printf("\n\nBai 4.4NC/ Danh sach cac so chan nho hon hoac bang %d la: ", n);
         // khi i <= n thì kiểm tra i có phải số chẵn không, nếu có thì in ra sau đó i tăng thêm 1 đến khi i > n
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0)
                 System.out.print("\t" + i);
             }
         }
@@ -87,8 +97,10 @@ public class Buoi4
     {
         System.out.printf("\n\nBai 4.5NC/ Danh sach cac so le nho hon %d la: ", n);
         // khi i <= n thì kiểm tra i có phải số lẻ không, nếu có thì in ra sau đó i tăng thêm 1 đến khi i = n
-        for (int i = 1; i < n; i++) {
-            if (i % 2 != 0) {
+        for (int i = 1; i < n; i++)
+        {
+            if (i % 2 != 0)
+            {
                 System.out.print("\t" + i);
             }
         }
@@ -98,8 +110,10 @@ public class Buoi4
     {
         System.out.printf("\n\nBai 4.6NC/ Danh sach cac so chan nho hon %d la: ", n);
         // khi i <= n thì kiểm tra i có phải số chẵn không, nếu có thì in ra sau đó i tăng thêm 1 đến khi i = n
-        for (int i = 1; i < n; i++) {
-            if (i % 2 == 0) {
+        for (int i = 1; i < n; i++)
+        {
+            if (i % 2 == 0)
+            {
                 System.out.print("\t" + i);
             }
         }
