b0fc4ff3b33ece29aa38c4cabecbd52b9d3c519e
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-10-30 14:34:58 +0700

    Add function named nhapDiem() and nhapTuoi()

--- src/Buoi7/Buoi7.java
+++ src/Buoi7/Buoi7.java
@@ -4,6 +4,31 @@ import java.util.Scanner;
 
 public class Buoi7
 {
+    public static int nhapDiem()
+    {
+        Scanner sc = new Scanner(System.in);
+        int diem = 0;
+
+        while (true)
+        {
+            try
+            {
+                System.out.print("Nhap tuoi: ");
+                diem = sc.nextInt();
+                if (diem >= 0 && diem <= 10) break;
+                else
+                {
+                    System.out.println("So diem khong hop le, diem nam trong khoang 0-10. Vui long nhap lai!");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.out.println("So diem khong hop le, diem nam trong khoang 0-10. Vui long nhap lai!");
+            }
+        }
+        return diem;
+    }
+
     public static int nhapTuoi()
     {
         Scanner sc = new Scanner(System.in);
