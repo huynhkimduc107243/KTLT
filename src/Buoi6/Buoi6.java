5539c62fe5aa358319d9beaccdcf5ab249ca115b
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-10-30 15:11:17 +0700

    Complete exercise week 6 in Buổi 6.md

--- src/Buoi6/Buoi6.java
+++ src/Buoi6/Buoi6.java
@@ -14,13 +14,19 @@ public class Buoi6 {
     }
 
     public static void truongHop2(String[] args) {
-        String hoTen = "Nguyễn Văn An";
+        String hoTen = "Nguyen Van An";
         String ngaySinh = "20/10/2006";
         Float diemTongKet = 5.5f;
         int tuoi = 18;
+
+        System.out.println("\nHo ten: " + hoTen);
+        System.out.println("Ngay sinh: " + ngaySinh);
+        System.out.println("Diem tong ket: " + Float.toString(diemTongKet));
+        System.out.println("Tuoi: " + Integer.toString(tuoi));
     }
 
     public static void main(String[] args) {
         truongHop1(args);
+        truongHop2(args);
     }
 }
