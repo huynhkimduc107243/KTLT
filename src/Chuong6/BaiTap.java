d9ca0bb454c1fd71a69353c3aebd0c47ec994c02
Huynh Kim Duc <duc107243@donga.edu.vn>
2024-11-13 13:07:07 +0700

    Edit comments in BaiTap.java

--- src/Chuong6/BaiTap.java
+++ src/Chuong6/BaiTap.java
@@ -17,8 +17,9 @@ public class BaiTap
         for (int hang = 0; hang < chieuCao; hang++)
         {
             /*
-            Số lần in dấu sao cho mỗi hàng chính là hang + 1 vì hàng 0 cần 1 dấu sao, hàng 1 cần 2 dấu sao, v.v.
-             Cụ thể: Hàng đầu tiên: hang = 0, chỉ có 1 cột cot = 0 được in dấu sao. Hàng thứ 2: hang = 1, sẽ có 2 cột cot = 0 và 1 được in dấu sao, v.v.
+            Số lần in dấu sao cho mỗi hàng là hang + 1 vì hàng 0 cần 1 dấu sao, hàng 1 cần 2 dấu sao, v.v.
+             Cụ thể: Hàng đầu tiên: hang = 0, chỉ có 1 cột cot = 0 được in dấu sao.
+                     Hàng thứ 2: hang = 1, sẽ có 2 cột cot = 0 và 1 được in dấu sao, v.v.
             */
             for (int cot = 0; cot < hang + 1; cot++)
             {
@@ -33,8 +34,7 @@ public class BaiTap
     {
         /*
         Duyệt qua từng hàng, bắt đầu từ hàng cuối cùng (chieuCao - 1) và giảm dần
-         Ví dụ: nếu chiều cao tam giác là 5, thì cot sẽ được gán giá trị 5 - 1 = 4, tức là bắt đầu từ hàng 4 (hàng cuối cùng trong tam giác vuông gốc)
-         Dừng sau khi hang giảm xuống 0 vì duyệt từ dưới lên trên
+         Ví dụ: nếu chieuCao là 3, thì cot sẽ được gán giá trị 3 - 1 = 2, tức là bắt đầu từ hang 2 (hàng cuối cùng trong tam giác vuông gốc)
         */
         for (int hang = chieuCao - 1; hang >= 0; hang--)
         {
